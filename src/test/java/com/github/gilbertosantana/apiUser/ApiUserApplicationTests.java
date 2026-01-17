04831a6cbf250182cfd1f43f9a184802f557f15f
gilbertosantana <gilbertosantoss307@gmail.com>
2026-01-16 22:13:35 -0300

    Project created

---
+++ src/test/java/com/github/gilbertosantana/apiUser/ApiUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gilbertosantana.apiUser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
